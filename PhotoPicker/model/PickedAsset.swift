59dfe4f0ecd0c014148727b26ffac83d37670a1c
musicode <musicode@qq.com>
2023-09-18 11:03:33 +0800

    返回的图片新增 base64

--- PhotoPicker/model/PickedAsset.swift
+++ PhotoPicker/model/PickedAsset.swift
@@ -6,18 +6,21 @@ import Photos
     
     @objc public var path: String
     
+    @objc public var base64: String
+    
     @objc public var width: Int
     
     @objc public var height: Int
     
     @objc public var size: Int
-    
+
     @objc public var isVideo: Bool
     
     @objc public var isRaw: Bool
     
-    public init(path: String, width: Int, height: Int, size: Int, isVideo: Bool, isRaw: Bool) {
+    public init(path: String, base64: String, width: Int, height: Int, size: Int, isVideo: Bool, isRaw: Bool) {
         self.path = path
+        self.base64 = base64
         self.width = width
         self.height = height
         self.size = size
